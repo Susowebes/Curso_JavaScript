96ed9fdaa179fd478d0b3f629e5089769c8692cf
Susoweb <susowebes@gmail.com>
2023-03-21 12:04:46 +0000

    Proyecto usoPoo listo

---
+++ POO/JS/usoPoo.js
@@ -0,0 +1,12 @@
+//Creamos un objeto con 2 propiedades.
+let coche={color:"rojo",potencia:400};
+
+// Metodo para ver el código directamente el pa página. 
+// document.write("El color del coche es "+coche.color);
+
+// Metodo para ver el código dentro de un elemento con un "id" seleccionado.
+// document.getElementById("codigo").innerHTML = coche.color +"<br>"+coche.potencia;
+
+/* Metodo para ver el código en la consola "F12". Incluyendo solo el nombre del objeto 
+   se conocen todos sus propiedades y sus herencias. */
+console.log(coche);
