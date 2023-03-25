c66b223525860eea3f639c595bbe04fd87e9c90f
Susoweb <127110440+Susowebes@users.noreply.github.com>
2023-03-25 00:06:09 +0000

    Actualizar valueOf.js

--- POO/JS/valueOf.js
+++ POO/JS/valueOf.js
@@ -7,7 +7,7 @@ function ValueOf(){
     // Con "valueOf()" averiguamos cual es su valor.
     console.log(fecha);
     console.log(fecha.valueOf());
-    document.getElementById("titulo").innerHTML="Con una fecha (2023,03,24):"
+    document.getElementById("titulo").innerHTML="Con una fecha (2023,03,24)";
     document.getElementById("codigo").innerHTML=fecha.valueOf();
     
     // Creamos un string.
@@ -16,6 +16,6 @@ function ValueOf(){
     // Con "valueOf()" averiguamos cual es su valor.
     console.log(curso);
     console.log(curso.valueOf());
-    document.getElementById("titulo2").innerHTML='Con un "String":'
+    document.getElementById("titulo2").innerHTML='Con un "String".';
     document.getElementById("codigo2").innerHTML=curso;
 }
